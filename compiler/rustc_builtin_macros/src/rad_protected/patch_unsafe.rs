14f0e11df717b657b4f74402ea153d0f26f79825
Ezlanding1 <113404035+Ezlanding1@users.noreply.github.com>
2026-06-22 23:12:47 -0400

    Add MutVisitor to track unsafe blocks in AST

--- compiler/rustc_builtin_macros/src/rad_protected/patch_unsafe.rs
+++ compiler/rustc_builtin_macros/src/rad_protected/patch_unsafe.rs
@@ -1,6 +1,32 @@
 use rustc_ast as ast;
+use rustc_ast::mut_visit::{self, MutVisitor};
 use rustc_expand::base::ExtCtxt;
+use rustc_span::{symbol::Ident, DUMMY_SP};
+use thin_vec::thin_vec;
 
-pub(crate) fn patch_unsafe_blocks(_cx: &ExtCtxt<'_>, _body: &mut ast::Block) {
+pub(crate) fn patch_unsafe_blocks(cx: &ExtCtxt<'_>, body: &mut ast::Block) {
+    let mut visitor = UnsafeBlockRewriter { cx };
+    visitor.visit_block(body);
+}
+
+struct UnsafeBlockRewriter<'a, 'cx> {
+    cx: &'a ExtCtxt<'cx>,
+}
+
+impl MutVisitor for UnsafeBlockRewriter<'_, '_> {
+    fn visit_expr(&mut self, expr: &mut ast::Expr) {
+
+        if let ast::ExprKind::Block(block, _) = &mut expr.kind {
+            if matches!(block.rules, ast::BlockCheckMode::Unsafe(_)) {
+                patch_unsafe_block(self.cx, block);
+                return;
+            }
+        }
+
+        mut_visit::walk_expr(self, expr);
+    }
+}
+
+fn patch_unsafe_block(cx: &ExtCtxt<'_>, block: &mut ast::Block) {
     todo!();
 }
