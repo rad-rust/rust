4e71a29b9d9a47032cfefde98ffebbd862fc8ea5
Ezlanding1 <113404035+Ezlanding1@users.noreply.github.com>
2026-07-17 17:24:19 -0400

    Move rad_protected to its own library, exported through std

---
+++ library/rad_protected/src/lib.rs
@@ -0,0 +1,21 @@
+//! The Rad-Rust `rad_protected` Runtime
+//! 
+//! This library contains a static runtime for running software radiation-hardened programs through TMR.
+//! 
+//! The runtime is intended to be used with a rustc fork to generate runtime calls,
+//! and thus only meant to be referenced directly by the compiler itself.
+//! 
+//! For porting this library to a new target, see mini_std/mod.rs
+
+#![feature(staged_api)]
+#![allow(internal_features)]
+#![no_std]
+
+/// `rad_protected` runtime module
+#[stable(feature = "rad_protected", since = "1.95.0")]
+pub mod runtime;
+
+mod fork;
+mod libc_helpers;
+mod role;
+mod mini_std;
--- library/std/src/rad_protected/mod.rs
+++
@@ -1,8 +0,0 @@
-/// `rad_protected` runtime module
-#[stable(feature = "rad_protected", since = "1.95.0")]
-pub mod runtime;
-
-mod fork;
-mod libc_helpers;
-mod role;
-mod mini_std;
